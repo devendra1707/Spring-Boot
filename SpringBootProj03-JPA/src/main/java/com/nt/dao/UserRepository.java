763ff03c63fb4c8b68089020d78eace386188b65
Devendra Kumar Patel <92505396+Devendra-Octto@users.noreply.github.com>
2022-07-16 20:35:51 +0530

    Revert "Spring Boot Program"

--- SpringBootProj03-JPA/src/main/java/com/nt/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nt.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nt.entities.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
